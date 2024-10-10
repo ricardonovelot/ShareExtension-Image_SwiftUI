e8625b805b76689c9a276a004c29e56ab64179b6
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-10-10 11:07:08 -0600

    Test

--- ShareImage/ShareViewController.swift
+++ ShareImage/ShareViewController.swift
@@ -1,3 +1,10 @@
+//
+//  ShareViewController.swift
+//  ShareImage
+//
+//  Created by Ricardo on 08/10/24.
+//
+
 import UIKit
 import Social
 import UniformTypeIdentifiers
@@ -8,6 +15,7 @@ class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Ensure access to extensionItem and itemProvider
         guard
             let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
             let itemProvider = extensionItem.attachments?.first else {
@@ -15,37 +23,44 @@ class ShareViewController: UIViewController {
             return
         }
         
-        // Check for image type
-        if itemProvider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-            print("Data type accepted")
-            
-            itemProvider.loadItem(forTypeIdentifier: UTType.image.identifier, options: nil) { (providedItem, error) in
-                if let error = error {
-                    print("Error loading image item: \(error.localizedDescription)")
-                    self.close()
-                    return
-                }
+        // Check type identifier
+        let imageDataType = UTType.image.identifier
+        if itemProvider.hasItemConformingToTypeIdentifier(imageDataType) {
+            printContent("data type accepted")
+            // Load the item from itemProvider
+                   itemProvider.loadItem(forTypeIdentifier: imageDataType , options: nil) { (providedImage, error) in
+                       if let error = error {
+                           print("Error loading text item: \(error.localizedDescription)")
+                           self.close()
+                           return
+                       }
 
-                if let image = providedItem as? UIImage {
-                    // Handle UIImage
-                    DispatchQueue.main.async {
-                        self.showSwiftUIView(with: image)
-                    }
-                } else if let imageUrl = providedItem as? URL {
-                    // Load UIImage from URL
-                    if let data = try? Data(contentsOf: imageUrl), let imageFromUrl = UIImage(data: data) {
-                        DispatchQueue.main.async {
-                            self.showSwiftUIView(with: imageFromUrl)
-                        }
-                    } else {
-                        self.close()
-                    }
-                } else {
-                    self.close()
-                }
-            }
+                       
+                       if let image = providedImage as? UIImage {
+                           // if we get here, we're good and can show the View :D
+                           DispatchQueue.main.async { // y tell that we want the code responsible for showing the view to run on the main thread with DispatchQueue.main.async , because the loadItem closure can be executed on background threads.
+                                               // host the SwiftU view
+                                               let contentView = UIHostingController(rootView: ShareExtensionView(image: image))
+                                               self.addChild(contentView)
+                                               self.view.addSubview(contentView.view)
+                                               
+                                               // set up constraints
+                                               contentView.view.translatesAutoresizingMaskIntoConstraints = false
+                                               contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+                                               contentView.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
+                                               contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+                                               contentView.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
+                                           }
+                           
+                       } else {
+                           self.close()
+                           return
+                       }
+                   }
+            
         } else {
             close()
+            return
         }
         
         NotificationCenter.default.addObserver(forName: NSNotification.Name("close"), object: nil, queue: nil) { _ in
@@ -55,24 +70,9 @@ class ShareViewController: UIViewController {
         }
     }
     
-    /// Show the SwiftUI View using the passed image
-    func showSwiftUIView(with image: UIImage) {
-        let contentView = UIHostingController(rootView: ShareExtensionView(image: image))
-        self.addChild(contentView)
-        self.view.addSubview(contentView.view)
-        
-        // Set up constraints
-        contentView.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor),
-            contentView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            contentView.view.rightAnchor.constraint(equalTo: self.view.rightAnchor)
-        ])
-    }
-
     /// Close the Share Extension
     func close() {
         self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
+
 }
